74269029b7259bd6728d54bfef805c54900654e6
icegergert-world <andrejkornev26@gmail.com>
2025-05-10 18:31:00 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,60 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() : countOp(0), first(nullptr) {}
+
+Train::~Train() {
+    if (first != nullptr) {
+        Car* current = first;
+        do {
+            Car* toDelete = current;
+            current = current->next;
+            delete toDelete;
+        } while (current != first);
+    }
+}
+
+void Train::addCar(bool lit) {
+    Car* newWagon = new Car(lit);
+    if (first == nullptr) {
+        first = newWagon;
+        first->next = first;
+        first->prev = first;
+    }
+    else {
+        Car* tail = first->prev;
+        tail->next = newWagon;
+        newWagon->prev = tail;
+        newWagon->next = first;
+        first->prev = newWagon;
+    }
+}
+
+int Train::getLength() {
+    first->light = true;
+    Car* current = first->next;
+
+    while (true) {
+        int length = 1;
+        ++countOp;
+        while (!current->light) {
+            current = current->next;
+            ++countOp;
+            ++length;
+        }
+        current->light = false;
+
+        for (int i = 0; i < length; ++i) {
+            current = current->prev;
+            ++countOp;
+        }
+
+        if (!current->light) {
+            return length;
+        }
+        current = current->next;
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
